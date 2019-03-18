836139a0b5161e89f6e566fffd83f2c2353fd2f0
tjforeman <tylerforeman92@gmail.com>
2019-03-18 18:42:33 -0400

    staged changes

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent= () =>{
+    return(
+        <div className='card-content'>
+        <h3>Get started with React</h3>
+        <h6>React makes it painless to create interactive UIs. Design simple views for each state in your application</h6>
+        <a href=''>reactjs.org</a>
+        </div>
+    );
+}
+export default CardContent
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent'
+
+const HeaderContainer= () => {
+    return (<div class='header-container'>
+    <ImageThumbnail />
+    <HeaderContent />
+    </div>
+    );
+}
+export default HeaderContainer;
