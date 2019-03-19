9d2713cb3a04ba641e3b5f47c98c185c2d3af0f2
tjforeman <tylerforeman92@gmail.com>
2019-03-18 22:11:03 -0400

    updated styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitles.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitles.js
@@ -2,6 +2,6 @@ import React from 'react';
 import './Header.css';
 
 let  HeaderTitle= () =>{
-    return <div className="header-title-container"><h2 className="header-title">Lambda School</h2><p className="header-email">@LambdaSchool</p><p className="header-date">jan 26</p></div>
+    return <div className="header-title-container"><h2 className="header-title">Lambda School</h2><p className="header-email">@LambdaSchool</p><p className="header-date">26 Jan</p></div>
   };
   export default HeaderTitle;
